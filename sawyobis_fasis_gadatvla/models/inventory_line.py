58db18b9b0fccccd352614c60b5745565c3f8def
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-05-07 12:20:22 +0400

    sawyobis fasisi gadatvla

--- sawyobis_fasis_gadatvla/models/inventory_line.py
+++ sawyobis_fasis_gadatvla/models/inventory_line.py
@@ -28,3 +28,28 @@ class InventoryLine(models.Model):
             else:
                 line.unit_price_xelsh = 0.0
                 line.amount_xelsh = 0.0
+
+    def _create_purchase_agreement_direct(self, lines, vendor, purchase_method=None):
+        """Override price/amount sources for direct agreement creation."""
+        agreement = super()._create_purchase_agreement_direct(lines, vendor, purchase_method=purchase_method)
+
+        line_model = self.env["purchase.requisition.line"]
+        has_total_amount = "total_amount" in line_model._fields
+
+        for agreement_line in agreement.line_ids:
+            inventory_line = agreement_line.inventory_line_ids[:1]
+            if not inventory_line:
+                continue
+
+            price = inventory_line.unit_price_xelsh
+            vals = {"price_unit": price}
+
+            if has_total_amount:
+                vals["total_amount"] = agreement_line.product_qty * price
+
+            agreement_line.write(vals)
+
+        if "requested_amount" in agreement._fields:
+            agreement.write({"requested_amount": sum(lines.mapped("amount_xelsh"))})
+
+        return agreement
