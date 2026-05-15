25cf2d69facf6531d693339e7802cc135f1668cf
Luka Goderdzishvili <lukagoderdzishvili4@gmail.com>
2026-05-15 16:14:01 +0400

    done factura daily

--- done_factura_daily/__manifest__.py
+++ done_factura_daily/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0.0',
     'category': 'Accounting',
     'summary': 'Daily cron: auto-create done.factura from avansi for active contracts',
-    'depends': ['done_factura_download_module'],
+    'depends': ['done_factura_download_module', 'purchase_requisition', 'account'],
     'data': [
         'data/cron.xml',
     ],
